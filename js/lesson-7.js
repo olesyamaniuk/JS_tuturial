20d4201448eb78d332f17f280377245cc07d43fe
olesyamaniuk <maniukolesya222@gmail.com>
2024-02-14 12:50:01 +0200

    7

---
+++ js/lesson-7.js
@@ -0,0 +1,16 @@
+console.log(document);
+
+const body = document.body;
+console.log(body);
+
+const list = document.querySelector(".list");
+console.log(list);
+
+const firstListItem = list.firstElementChild;
+console.log(firstListItem);
+
+const lastListItem = list.lastElementChild;
+console.log(lastListItem);
+
+const listItems = list.children;
+console.log(listItems);
