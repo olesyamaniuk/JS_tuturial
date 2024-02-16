3caef256bf147db5b6165119d4fb6c9e5676b64d
olesyamaniuk <maniukolesya222@gmail.com>
2024-02-16 17:04:01 +0200

    7

--- js/lesson-7.js
+++ js/lesson-7.js
@@ -1,10 +1,16 @@
 console.log(document);
 
-const body = document.body;
-console.log(body);
+// const body = document.body;
+// console.log(body);
 
 const list = document.querySelector(".list");
 console.log(list);
+
+// const lists = document.querySelectorAll('li');
+// console.log(lists);
+
+const parentNode = list.parentNode;
+console.log(parentNode);
 
 const firstListItem = list.firstElementChild;
 console.log(firstListItem);
@@ -14,3 +20,153 @@ console.log(lastListItem);
 
 const listItems = list.children;
 console.log(listItems);
+// ---------------------------------------------------------
+const link = document.querySelector(".link");
+console.log(link.href); // "<https://goit.global>"
+
+link.href = "[<https://neo.goit.global>](<https://neo.goit.global/>)";
+console.log(link.href); // "[<https://neo.goit.global>](<https://neo.goit.global/>)"
+
+
+const el = document.querySelector(".text")
+const nested = document.querySelector(".sub-text")
+
+console.log(el.textContent); // "Username: Mango"
+console.log(nested.textContent); // "Mango"
+
+el.textContent = "Username: Poly";
+console.log(el.textContent);
+
+console.log(link.classList); // ["link", "is-active", length: 2, value: "link is-active"]
+
+const hasActiveClass = link.classList.contains("is-active"); // true
+const hasActiveClasss = link.classList.contains("title"); // false
+console.log(hasActiveClass);
+console.log(hasActiveClasss);
+
+link.classList.add("special");
+console.log(link.classList); // ["link", "is-active", "special", length: 3, value: "link is-active special"]
+
+link.classList.remove("is-active");
+console.log(link.classList); // ["link", "special", length: 2, value: "link special"]
+
+link.classList.toggle("is-active");
+console.log(link.classList); // ["link", "special", "is-active", length: 3, value: "link special is-active"]
+
+link.classList.replace("special", "regular");
+console.log(link.classList); // ["link", "regular", "is-active", length: 3, value: "link regular is-active"]
+// -----------------------------------------------------------------------------
+
+const button = document.querySelector(".btn");
+
+button.style.backgroundColor = "teal";
+button.style.fontSize = "24px";
+button.style.textAlign = "center";
+
+console.log(button.style); // inline styles object
+// ---------------------------------------------------------------------------
+
+const image = document.querySelector(".image");
+console.log(image.hasAttribute("src")); // true
+console.log(image.hasAttribute("href")); // false
+
+console.log(image.getAttribute("alt")); // "Rocks and waterfall"
+
+image.setAttribute("alt", "Amazing nature");
+console.log(image.getAttribute("alt")); // Amazing nature
+
+image.removeAttribute("alt");
+console.log(image.hasAttribute("alt")); // false
+    
+// ---------------------------------------------------------------------------------
+const saveBtn = document.querySelector('button[data-action="save"]');
+console.log(saveBtn.dataset.action); // "save"
+
+const closeBtn = document.querySelector('button[data-action="close"]');
+console.log(closeBtn.dataset.action); // "close"
+
+// Змінюємо значення data-action для кнопки saveBtn
+saveBtn.dataset.action = "update";
+
+// Додаємо новий data-атрибут data-role
+saveBtn.dataset.role = "admin";
+
+// Перевіримо нові значення
+console.log(saveBtn.dataset.action); // "update"
+console.log(saveBtn.dataset.role); // "admin"
+// ------------------------------------------------------------------------
+const heading = document.createElement("h1");
+heading.classList.add("title");
+heading.textContent = "This is a heading";
+console.log(heading); // <h1 class="title">This is a heading</h1>
+
+const imagee = document.createElement("img");
+// imagee.src = "<https://picsum.photos/id/11/320/240>";
+imagee.alt = "Nature";
+console.log(imagee); // <img src="<https://picsum.photos/id/11/320/240>" alt="Nature" />
+// ------------------------------------------------------------------
+
+const lists = document.querySelector(".usernames");
+
+// Adds an item to the end of the list
+const lastItem = document.createElement("li");
+lastItem.textContent = "Poly";
+lists.append(lastItem);
+
+// Adds an item to the beginning of the list
+const firstItem = document.createElement("li");
+firstItem.textContent = "Ajax";
+lists.prepend(firstItem);
+
+// Adds a title before the list
+const title = document.createElement("h2");
+title.textContent = "USERNAMES";
+lists.before(title);
+
+// Adds a paragraph after the list
+const text = document.createElement("p");
+text.textContent =
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum tenetur assumenda fugiat maxime, soluta aspernatur quasi nihil in asperiores ad distinctio illo et debitis error iure voluptate numquam maiores nisi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum tenetur assumenda fugiat maxime, soluta aspernatur quasi nihil in asperiores ad distinctio illo et debitis error iure voluptate numquam maiores nisi!";
+lists.after(text);
+
+const textt = document.querySelector(".textt")
+textt.remove();
+// ---------------------------------------------------------------------------
+const article = document.querySelector(".article");
+console.log(article.innerHTML);
+
+const titlee = document.querySelector(".article .titlee");
+console.log(titlee.innerHTML);
+
+const texttt = document.querySelector(".article .texttt");
+console.log(texttt.innerHTML);
+
+const linkkk = document.querySelector(".article .linkkk");
+console.log(linkkk.innerHTML);
+
+titlee.innerHTML = 'New and <span class="accent">improved</span> title';
+// -----------------------------------------------------------------------------
+
+
+const technologies = ["HTML", "CSS", "JavaScript", "React", "Node"];
+const listq = document.querySelector(".listq");
+
+const markup = technologies
+  .map((technology) => `<li class="list-item">${technology}</li>`)
+  .join("");
+
+// Check the console, you'll see a single string with HTML tags
+console.log(markup);
+
+// Adding all the markup in one operation
+listq.innerHTML = markup;
+// ----------------------------------------------------------------------
+const listw = document.querySelector(".listw");
+
+const newTechnologies = ["React", "TypeScript", "Node.js"];
+const markupe = newTechnologies
+  .map((technology) => `<li class="list-item new">${technology}</li>`)
+  .join("");
+
+listw.insertAdjacentHTML("beforeend", markupe);
+listw.insertAdjacentHTML("beforebegin", "<h2>Popular technologies</h2>");
