5ad50d5962e7881117a5ae4199bc647b76534ed7
olesyamaniuk <maniukolesya222@gmail.com>
2024-02-27 14:40:25 +0200

    10

--- js/lesson-10.js
+++ js/lesson-10.js
@@ -126,6 +126,43 @@
 // const date = new Date('2022-5-22');
 // console.log(date);
 
+const date = new Date("March 16, 2030 14:25:00");
+console.log("Date: ", date); // "Sat Mar 16 2030 14:25:00 GMT+0200"
+
+// Повертає день місяця від 1 до 31
+console.log("Day: ", date.getDate()); // 16
+
+// Повертає день тижня від 0 до 6, починається з неділі
+console.log("Day of the week: ", date.getDay()); // 6
+
+// Повертає місяць від 0 до 11
+console.log("Month: ", date.getMonth()); // 2
+
+// Повертає рік з 4 цифр
+console.log("Full year: ", date.getFullYear()); // 2030
+
+// Повертає години
+console.log("Hours: ", date.getHours()); // 14
+
+// Повертає хвилини
+console.log("Minutes: ", date.getMinutes()); // 25
+
+// Повертає секунди
+console.log("Seconds: ", date.getSeconds()); // 0
+
+// Повертає мілісекунди
+console.log("Milliseconds: ", date.getMilliseconds()); // 0
+
+
+date.setMinutes(50); 
+console.log(date); // "Sat Mar 16 2030 14:50:00 GMT+0200"
+
+date.setFullYear(2040);
+console.log(date); // "Fri Mar 16 2040 14:50:00 GMT+0200"
+
+date.setMonth(4); 
+console.log(date); // "Wed May 16 2040 14:50:00 GMT+0300"
+
 // -----------------------------------------------------------------------------------------------------------------
 
 /**
