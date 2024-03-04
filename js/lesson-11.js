a101ad4ec8df8082b0076caf4010ab6286d8dd32
olesyamaniuk <maniukolesya222@gmail.com>
2024-03-04 11:43:40 +0200

    Update lesson-11.js

--- js/lesson-11.js
+++ js/lesson-11.js
@@ -50,7 +50,7 @@ fetch('https://jsonplaceholder.typicode.com/todos')
  */
 
 // оце з документації
-const KEY = '42457252-8d703b698c8e019830d95e145';
+const KEY = '42676723-0286cb816b5743467714b50d3';
 const BASE_URI = 'https://pixabay.com/api/';
 const QUERY = 'forest+mountains'
 // такий загальний вигляд посилання
@@ -92,128 +92,128 @@ getImages().then(data => {
  * Read (GET)
  */
 
-const apiUrl = "https://65d63786f6967ba8e3bdba79.mockapi.io/api/v1/";
+// const apiUrl = "https://65d63786f6967ba8e3bdba79.mockapi.io/api/v1/";
 
-function fetchBooks() {
-  return fetch(apiUrl + 'books')
-    .then((res) => res.json())
-}
-
-function fetchBookById(bookId) {
-  return fetch(`${apiUrl}books/${bookId}`)
-    .then((res) => res.json())
-}
-
-// fetchBooks();
-// fetchBookById(2);
-// fetchBookById(4);
-
-/**
- * Create (POST)
- */
-
-function addBook(book) {
-  const options = {
-    method: 'POST',
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(book)
-  }
-
-  // console.log(book);
-
-  return fetch(apiUrl + 'books', options).then(res => res.json())
-}
-
-// addBook({
-//   title: "Тестова книга з CSS",
-//   author: "Я",
-//   genres: ["CSS"],
-//   Rating: 9,
-// })
-//   .then((book) => {
-//     console.log("Прийшла відповідь від бекенда можна малювати");
-//     console.log(book);
-//   })
-//   .catch((error) => console.log(error));
-
-// addBook({
-//   title: "Тестова книга з HTML",
-//   author: "Я",
-//   genres: ["HTML"],
-//   Rating: 7,
-// }).then((book) => {
-//   console.log("Прийшла відповідь від бекенда можна малювати");
-//   console.log(book);
-// });
-
-/**
- * Update (PUT/PATCH)
- */
-
-function updateBookById(update, bookId) {
-  const options = {
-    method: 'PUT',
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(update)
-  }
-
-
-  return fetch(`${apiUrl}books/${bookId}`, options).then(response => response.json())
-}
-
-// updateBookById({ title: "Велика нова книга по NODEJS" }, 19);
-// updateBookById({ rating: 1 }, 18);
-// updateBookById({ rating: 4, author: "Манго" }, 17);
-
-/**
- * Delete (DELETE)
- */
-
-// function removeBook(bookId) {
-//   const config = {
-//     method: 'DELETE'
-//   }
-
-//   return fetch(`${apiUrl}books/${bookId}`, config).then(res => res.json())
-
+// function fetchBooks() {
+//   return fetch(apiUrl + 'books')
+//     .then((res) => res.json())
 // }
 
-// removeBook(15);
-// removeBook(14);
+// function fetchBookById(bookId) {
+//   return fetch(`${apiUrl}books/${bookId}`)
+//     .then((res) => res.json())
+// }
+
+// // fetchBooks();
+// // fetchBookById(2);
+// // fetchBookById(4);
+
+// /**
+//  * Create (POST)
+//  */
+
+// function addBook(book) {
+//   const options = {
+//     method: 'POST',
+//     headers: {
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify(book)
+//   }
+
+//   // console.log(book);
+
+//   return fetch(apiUrl + 'books', options).then(res => res.json())
+// }
+
+// // addBook({
+// //   title: "Тестова книга з CSS",
+// //   author: "Я",
+// //   genres: ["CSS"],
+// //   Rating: 9,
+// // })
+// //   .then((book) => {
+// //     console.log("Прийшла відповідь від бекенда можна малювати");
+// //     console.log(book);
+// //   })
+// //   .catch((error) => console.log(error));
+
+// // addBook({
+// //   title: "Тестова книга з HTML",
+// //   author: "Я",
+// //   genres: ["HTML"],
+// //   Rating: 7,
+// // }).then((book) => {
+// //   console.log("Прийшла відповідь від бекенда можна малювати");
+// //   console.log(book);
+// // });
+
+// /**
+//  * Update (PUT/PATCH)
+//  */
+
+// function updateBookById(update, bookId) {
+//   const options = {
+//     method: 'PUT',
+//     headers: {
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify(update)
+//   }
 
 
-const ENDPOINT = 'https://65d63786f6967ba8e3bdba79.mockapi.io/api/v1/users';
+//   return fetch(`${apiUrl}books/${bookId}`, options).then(response => response.json())
+// }
+
+// // updateBookById({ title: "Велика нова книга по NODEJS" }, 19);
+// // updateBookById({ rating: 1 }, 18);
+// // updateBookById({ rating: 4, author: "Манго" }, 17);
+
+// /**
+//  * Delete (DELETE)
+//  */
+
+// // function removeBook(bookId) {
+// //   const config = {
+// //     method: 'DELETE'
+// //   }
+
+// //   return fetch(`${apiUrl}books/${bookId}`, config).then(res => res.json())
+
+// // }
+
+// // removeBook(15);
+// // removeBook(14);
 
 
-function saveNewUser(name, city){
-    const data = JSON.stringify({
-        name, city
-    })
-
-    const config = {
-        method: 'POST',
-        body: data,
-        headers: {
-            "Content-Type": "application/json"
-        }
-    }
-
-    return fetch(ENDPOINT, config).then(response => response.json())
-}
-
-function getAllUsers(){
-    return fetch(ENDPOINT).then(res => res.json())
-}
+// const ENDPOINT = 'https://65d63786f6967ba8e3bdba79.mockapi.io/api/v1/users';
 
 
-// saveNewUser('Alex', 'London').then(data => {
-//     console.log('User created!', data)
-// })
+// function saveNewUser(name, city){
+//     const data = JSON.stringify({
+//         name, city
+//     })
+
+//     const config = {
+//         method: 'POST',
+//         body: data,
+//         headers: {
+//             "Content-Type": "application/json"
+//         }
+//     }
+
+//     return fetch(ENDPOINT, config).then(response => response.json())
+// }
+
+// function getAllUsers(){
+//     return fetch(ENDPOINT).then(res => res.json())
+// }
 
 
-const a = getAllUsers();
-a.then((data) => console.log(data))
+// // saveNewUser('Alex', 'London').then(data => {
+// //     console.log('User created!', data)
+// // })
+
+
+// const a = getAllUsers();
+// a.then((data) => console.log(data))
